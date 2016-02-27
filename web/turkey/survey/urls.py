87347e2a839f844e008969a1aa35bf59f3dee500
Miles <mileshutson@gmail.com>
2016-02-27 15:22:43 -0600

    Add view code for task

--- web/turkey/survey/urls.py
+++ web/turkey/survey/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^submission/(?P<pk>[0-9]+)/$', views.RecordSubmission.as_view(),
         name='recordSubmission'),
+    url(r'^task/(?P<pk>[0-9]+)/$', views.TaskView.as_view(),
+        name='TaskPage')
 ]
--- web/turkey/survey/views.py
+++ web/turkey/survey/views.py
@@ -1,4 +1,5 @@
 from django.apps import apps
+from django.template.response import TemplateResponse
 from django.views.generic import View
 from rest_framework import status
 from rest_framework.views import APIView
@@ -24,6 +25,29 @@ class RecordSubmission(APIView):
         self.save_data_to_mapped_models(submission.steps, NAME_TO_STEP, task)
         return Response(status=status.HTTP_201_CREATED)
 
+
 class TaskView(View):
-    def get(self, request):
-        pass
+    def get(self, request, **kwargs):
+        task = Task.objects.get(pk=kwargs['pk'])
+        if task.number_simultaneous_users > 1:
+            # TODO: Support for lobby
+            raise Exception('Support for simultaneous users has not yet been coded')
+        if task.task_dependencies.count() > 0:
+            # TODO: Support for checking if user has completed previous tasks
+            raise Exception('Support for interdependent tasks has not yet been coded')
+
+        # TODO: Find a way to cut down on the number of queries these two for loops will have to make
+        auditors = []
+        for auditor_model_name in NAME_TO_AUDITOR.values():
+            auditor_model = apps.get_model('survey', auditor_model_name)
+            auditors.extend(auditor_model.objects.filter(task=task))
+
+        steps = []
+        for step_model_name in NAME_TO_STEP.values():
+            step_model = apps.get_model('survey', step_model_name)
+            steps.extend(step_model.objects.filter(task=task))
+        steps.sort(key=lambda x: x.step_num)
+
+        return TemplateResponse(request, task.survey_wrap_template, status=status.HTTP_200_OK,
+                                context={'auditors': auditors,
+                                         'steps': steps})
