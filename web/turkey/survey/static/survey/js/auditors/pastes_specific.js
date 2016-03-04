879e28ac388441e7cc1e43c57b88b696b78af9ba
uyvu <frogninja@sbcglobal.net>
2016-03-03 19:10:24 -0600

    paste-event auditors

---
+++ web/turkey/survey/static/survey/js/auditors/pastes_specific.js
@@ -0,0 +1,34 @@
+var AuditorPastesSpecific = {
+    pastes_specific: [],
+    log_paste_content: function (e) {
+        $("document").bind("paste", function(e) {
+            // e.stopPropagation();
+            var pasted_data = e.originalEvent.clipboardData.getData('text');
+            this.pastes_specific.push(pasted_data);
+        });
+    },
+    submit_callable: function () {
+        return {
+            'content':  this.pastes_specific != []
+                        ?
+                        this.pastes_specific
+                        :
+                        null
+        };
+    }
+};
+
+var auditor_pastes_specific = Object.create(AuditorPastesSpecific);
+
+$(document).ready(function() {
+    $(document).bind("paste", function(e) {
+        auditor_pastes_specific
+            .log_paste_content
+            .bind(auditor_pastes_specific
+    });
+});
+
+overlord.register_auditor('pastes_specific',
+                          auditor_pastes_specific
+                            .submit_callable
+                            .bind(auditor_pastes_specific));
---
+++ web/turkey/survey/static/survey/js/auditors/pastes_total.js
@@ -0,0 +1,29 @@
+var AuditorPastesTotal = {
+    pastes_total: 0,
+    log_paste_event: function (e) {
+        $("document").bind("paste", function(e) {
+            // e.stopPropagation();
+            this.pastes_total += 1;
+        });
+    },
+    submit_callable: function () {
+        return {
+            'count': this.pastes_total
+        };
+    }
+};
+
+var auditor_pastes_total = Object.create(AuditorPastesTotal);
+
+$(document).ready(function() {
+    $(document).bind("paste", function(e) {
+        auditor_pastes_total
+            .log_paste_event
+            .bind(auditor_pastes_total
+    });
+});
+
+overlord.register_auditor('pastes_total',
+                          auditor_pastes_total
+                            .submit_callable
+                            .bind(auditor_pastes_total));
