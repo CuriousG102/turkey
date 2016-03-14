0bcbfa4556585e6af5f1aefd77bf37bddbf8d7f0
uyvu <frogninja@sbcglobal.net>
2016-03-13 20:28:01 -0500

    merged horizontal and vertical scroll auditors into combined auditor and added setup functions; this currently works nicely, but I will look into throttling these as well

---
+++ web/turkey/survey/static/survey/js/auditors/scrolled_pixels_specifics.js
@@ -0,0 +1,48 @@
+var AuditorScrolledPixelsSpecific = {
+    scrolled_pixels_specific: [],
+    previous_position_h: 0,
+    previous_position_v: 0,
+    setup: function() {
+        this.previous_position_h = $(window).scrollLeft();
+        this.previous_position_v = $(window).scrollTop();
+    }
+    log_scroll_specific: function () {
+        // horizontal
+        var current_position_h = $(window).scrollLeft();
+        var raw_amount_h = current_position_h - previous_position_h;
+        var horizontal = { 'position' : current_position_h, 'change' : raw_amount_h };
+        previous_position_h = current_position_h;
+
+        // vertical
+        var current_position_v = $(window).scrollTop();
+        var raw_amount_v = current_position_v - previous_position_v;
+        var vertical = { 'position' : current_position_v, 'change' : raw_amount_v };
+        previous_position_v = current_position_v;
+
+        this.scrolled_pixels_specific.push(
+            {
+                'horizontal'    : horizontal,
+                'vertical'      : vertical
+            }
+        );
+    },
+    submit_callable: function () {
+        return {
+            'content':  this.scrolled_pixels_specific
+        };
+    }
+};
+
+var auditor_scrolled_pixels_specific = Object.create(AuditorScrolledPixelsSpecific);
+auditor_scrolled_pixels_specific.setup();
+
+$(document).ready(function() {
+    $(window).scroll(auditor_scrolled_pixels_specific
+                            .log_scroll_specific
+                            .bind(auditor_scrolled_pixels_specific));
+});
+
+overlord.register_auditor('scrolled_pixels_specific',
+                          auditor_scrolled_pixels_specific
+                            .submit_callable
+                            .bind(auditor_scrolled_pixels_specific));
---
+++ web/turkey/survey/static/survey/js/auditors/scrolled_pixels_total.js
@@ -0,0 +1,49 @@
+var AuditorScrolledPixelsTotal = {
+    scrolled_pixels_total_h: 0,
+    previous_position_h: 0,
+    scrolled_pixels_total_v: 0,
+    previous_position_v: 0,
+    setup: function() {
+        this.previous_position_h = $(window).scrollLeft();
+        this.previous_position_v = $(window).scrollTop();
+    },
+    log_scroll_event: function () {
+        // horizontal
+        var current_position_h = $(window).scrollLeft();
+        if(current_position_h != previous_position_h) {
+            var raw_amount_h = current_position_h - previous_position_h;
+            var amount_h = Math.abs(raw_amount_h);
+            this.scrolled_pixels_total_h += amount_h;
+            previous_position_h = current_position_h;
+        }
+
+        // vertical
+        var current_position_v = $(window).scrollTop();
+        if(current_position_v != previous_position_v) {
+            var raw_amount_v = current_position_v - previous_position_v;
+            var amount_v = Math.abs(raw_amount_v);
+            this.scrolled_pixels_total_v += amount_v;
+            previous_position_v = current_position_v;
+        }
+    },
+    submit_callable: function () {
+        return {
+            'horizontal': this.scrolled_pixels_total_h,
+            'vertical'  : this.scrolled_pixels_total_v
+        };
+    }
+};
+
+var auditor_scrolled_pixels_total = Object.create(AuditorScrolledPixelsTotal);
+auditor_scrolled_pixels_total.setup();
+
+$(document).ready(function() {
+    $(window).scroll(auditor_scrolled_pixels_total
+                            .log_scroll_event
+                            .bind(auditor_scrolled_pixels_total));
+});
+
+overlord.register_auditor('scrolled_pixels_total',
+                          auditor_scrolled_pixels_total
+                            .submit_callable
+                            .bind(auditor_scrolled_pixels_total));
