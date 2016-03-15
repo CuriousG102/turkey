54d2dc3b9fd09c515ae7f0d7f475c06b9cca3758
Miles <mileshutson@gmail.com>
2016-03-14 23:04:15 -0500

    Comment out non-implemented auditors. Guaranteed to provide the merge conflict of the year when I merge with Brandon's auditor code.

--- web/turkey/survey/auditors.py
+++ web/turkey/survey/auditors.py
@@ -9,27 +9,27 @@ from .models import Auditor, AuditorData
 NAME_TO_AUDITOR = {
     'total_task_time': 'AuditorTotalTaskTime',
     'before_typing_delay': 'AuditorBeforeTypingDelay',
-    'on_focus_time': '',
-    'within_typing_delay': '',
-    'recorded_time_disparity': '',
-    'clicks_total': '',
-    'clicks_specific': '',  # specific positions and (hopefully) elements clicked inside of
-    'keypresses_total': '',
-    # There are fields total tabs, total
-    # backspaces, and count of unique characters
-    # in the paper above, but these can
-    # be extracted from aggregations on
-    # keypresses_specific
-    'keypresses_specific': '',
-    'mouse_movement_total': '',
-    'mouse_movement_specific': '',
-    'pastes_total': '',
-    'pastes_specific': '',
-    'scrolled_vertical_pixels_total': '',  # total of position changes via scroll (absolute values)
-    'scrolled_horizontal_pixels_total': '',
-    'scrolled_vertical_pixels_specific': '',  # breakdown of specific_scrolled_pixels in time intervals
-    'scrolled_horizontal_pixels_specific': '',
-    'focus_changes': '',  # timestamped
+    # 'on_focus_time': '',
+    # 'within_typing_delay': '',
+    # 'recorded_time_disparity': '',
+    # 'clicks_total': '',
+    # 'clicks_specific': '',  # specific positions and (hopefully) elements clicked inside of
+    # 'keypresses_total': '',
+    # # There are fields total tabs, total
+    # # backspaces, and count of unique characters
+    # # in the paper above, but these can
+    # # be extracted from aggregations on
+    # # keypresses_specific
+    # 'keypresses_specific': '',
+    # 'mouse_movement_total': '',
+    # 'mouse_movement_specific': '',
+    # 'pastes_total': '',
+    # 'pastes_specific': '',
+    # 'scrolled_vertical_pixels_total': '',  # total of position changes via scroll (absolute values)
+    # 'scrolled_horizontal_pixels_total': '',
+    # 'scrolled_vertical_pixels_specific': '',  # breakdown of specific_scrolled_pixels in time intervals
+    # 'scrolled_horizontal_pixels_specific': '',
+    # 'focus_changes': '',  # timestamped
 }
 
 
