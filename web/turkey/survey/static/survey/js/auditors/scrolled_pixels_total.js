edc98d7b96169b3ad017ac366a9756d97cbeb588
uyvu <frogninja@sbcglobal.net>
2016-03-16 13:54:47 -0500

    phase 2 of debounce; made debounce js into a separate static file and added it to survey-default-template html

--- web/turkey/survey/static/survey/js/auditors/scrolled_pixels_total.js
+++ web/turkey/survey/static/survey/js/auditors/scrolled_pixels_total.js
@@ -6,9 +6,6 @@ var AuditorScrolledPixelsTotal = {
     setup: function() {
         this.previous_position_h = $(window).scrollLeft();
         this.previous_position_v = $(window).scrollTop();
-
-        // https://github.com/cowboy/jquery-throttle-debounce
-        (function(b,c){var $=b.jQuery||b.Cowboy||(b.Cowboy={}),a;$.throttle=a=function(e,f,j,i){var h,d=0;if(typeof f!=="boolean"){i=j;j=f;f=c}function g(){var o=this,m=+new Date()-d,n=arguments;function l(){d=+new Date();j.apply(o,n)}function k(){h=c}if(i&&!h){l()}h&&clearTimeout(h);if(i===c&&m>e){l()}else{if(f!==true){h=setTimeout(i?k:l,i===c?e-m:e)}}}if($.guid){g.guid=j.guid=j.guid||$.guid++}return g};$.debounce=function(d,e,f){return f===c?a(d,e,false):a(d,f,e!==false)}})(this);
     },
     log_scroll_event: function () {
         // horizontal
