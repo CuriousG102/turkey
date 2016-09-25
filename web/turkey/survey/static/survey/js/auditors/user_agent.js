d3b9107e5dcfe908b9c282c38f781e8d9fee750d
Brandon <budang@utexas.edu>
2016-09-24 19:56:51 -0500

    removing commented code

--- web/turkey/survey/static/survey/js/auditors/user_agent.js
+++ web/turkey/survey/static/survey/js/auditors/user_agent.js
@@ -3,22 +3,6 @@ var AuditorUserAgent = {
     uas: "",
     get_user_agent: function() {
         this.uas = navigator.userAgent;
-        // uas = encodeURI(uas);
-
-        // var url = "http://www.useragentstring.com/?uas="
-        //     + uas
-        //     + "&getJSON=all";
-
-        // $.ajax({
-        //     type: "GET",
-        //     url: url,
-        //     async: true,
-        //     dataType: "jsonp",
-        //     crossDomain: true,
-        //     success: function(data) {
-        //         this.ua = data;
-        //     }
-        // });
     },
     submit_callable: function() {
         return {
