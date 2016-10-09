5118988a8d5794d8bc1e60def0fbc4936ccf80ae
Miles Hutson <mileshutson@gmail.com>
2016-10-08 20:55:22 -0500

    Work with me here Travis

--- web/setup.sh
+++ web/setup.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 #these will error if there's already a database and user, but who cares...
-psql -h postgres -p 5432 -U postgres -c "CREATE DATABASE turkey;"
+psql -h postgres -p 5432 -U postgres -c "CREATE DATABASE turkey;";
 psql -h postgres -p 5432 -U postgres -c "
 CREATE USER django WITH PASSWORD 'django';
 ALTER ROLE django SET client_encoding TO 'utf8';
 ALTER ROLE django SET default_transaction_isolation TO 'read committed';
 ALTER ROLE django SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE turkey TO django;
+";
+psql -h postgres -p 5432 -U postgres -c "
+ALTER USER django CREATEDB;
 ";
---
+++ web/test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+bash setup.sh;
+python /usr/src/app/turkey/manage.py test;
