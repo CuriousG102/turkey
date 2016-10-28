fd2085e8f44cc83f6d9da8d2dc6f61b06ccc9d23
Miles Hutson <mileshutson@gmail.com>
2016-10-28 12:44:34 -0500

    Noticed that Travis can take up to 29 seconds so far to start selenium during testing, so upping the timeout to 60 seconds

--- web/test.sh
+++ web/test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-bash ./wait-for-it.sh -t 30 -h postgres -p 5432;
-bash ./wait-for-it.sh -t 30 -h selenium -p 4444;
+bash ./wait-for-it.sh -t 60 -h postgres -p 5432;
+bash ./wait-for-it.sh -t 60 -h selenium -p 4444;
 bash setup.sh;
 python /usr/src/app/turkey/manage.py test --liveserver=0.0.0.0:8082-8090 survey.tests;
