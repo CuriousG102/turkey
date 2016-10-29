d9bfda2b16fd5610ec7b8bd638cc8323a578d24c
Miles Hutson <mileshutson@gmail.com>
2016-10-28 23:51:53 -0500

    Parallelize for (hopefully) faster testing

--- web/test.sh
+++ web/test.sh
@@ -4,7 +4,7 @@ bash ./wait-for-it.sh -t 60 -h selenium -p 4444;
 bash setup.sh;
 if [ $# -eq 0 ]
 then
-    python /usr/src/app/turkey/manage.py test --liveserver=0.0.0.0:8082-8097 survey.tests;
+    python /usr/src/app/turkey/manage.py test --parallel --liveserver=0.0.0.0:8082-8097 survey.tests;
 else
     python /usr/src/app/turkey/manage.py test --liveserver=0.0.0.0:8082-8097 survey.tests.$1;
 fi
