23cf608c92940173542517c849dd7d681973af66
Miles Hutson <mileshutson@gmail.com>
2016-11-11 22:27:45 -0600

    quick fix

--- web/host-kernel.sh
+++ web/host-kernel.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+DEBUG=1;
+export DEBUG;
 python /usr/src/app/turkey/manage.py shell_plus --kernel&
 sleep 30s;
 cat /root/.local/share/jupyter/runtime/$(ls /root/.local/share/jupyter/runtime/);
