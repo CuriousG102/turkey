1d2b44109cc01abe43d3d18d3f688661df0264a9
Miles Hutson <mileshutson@gmail.com>
2016-11-11 23:13:05 -0600

    typos were made

--- web/host-kernel.sh
+++ web/host-kernel.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DEBUG=1;
 export DEBUG;
-python /usr/src/app/turkey/manage.py shell_plus --notebok --no-browser;
+python /usr/src/app/turkey/manage.py shell_plus --notebook --no-browser;
