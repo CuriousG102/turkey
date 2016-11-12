c7842727fb026644bace1b145fe240890862a69c
Miles Hutson <mileshutson@gmail.com>
2016-11-11 23:10:40 -0600

    A better idea

--- web/host-kernel.sh
+++ web/host-kernel.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 DEBUG=1;
 export DEBUG;
-python /usr/src/app/turkey/manage.py shell_plus --kernel&
-sleep 30s;
-cat /root/.local/share/jupyter/runtime/$(ls /root/.local/share/jupyter/runtime/);
+python /usr/src/app/turkey/manage.py shell_plus --notebok --no-browser;
