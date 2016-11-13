8a2d36d71876b056efcba6923617b4dd7ed48406
Miles Hutson <mileshutson@gmail.com>
2016-11-13 15:23:50 -0600

    Fix run-notebook-server

--- web/turkey/run-notebook-server.sh
+++ web/turkey/run-notebook-server.sh
@@ -4,5 +4,5 @@ DEBUG=1;
 export DEBUG;
 DJANGO_SETTINGS_MODULE=turkey.settings;
 export DJANGO_SETTINGS;
-openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mykey.key -out mycert.pem;
+openssl req -x509 -nodes -subj "/C=US/ST=TX/L=Austin/O=Me/CN=Meh" -days 365 -newkey rsa:1024 -keyout mykey.key -out mycert.pem;
 jupyter notebook --config=/usr/src/app/turkey/jupyter_config.py
