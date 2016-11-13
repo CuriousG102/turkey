c2b719c1cf1d01222292cb38a252db71b801c1f2
Miles Hutson <mileshutson@gmail.com>
2016-11-12 21:55:42 -0600

    Bajillionth time's the charm?

--- web/turkey/turkey/ipython_config.py
+++ web/turkey/turkey/ipython_config.py
@@ -3,7 +3,8 @@ import os
 c = get_config()
 
 # Allow all IP addresses to use the service and run it on port 8888.
-c.NotebookApp.ip = '0.0.0.0'
+print('test')
+c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 
 # Don't load the browser on startup.
