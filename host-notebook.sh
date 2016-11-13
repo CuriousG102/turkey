b95ab5838a7eca783179086d3aa5a7d22f3d37be
Miles Hutson <mileshutson@gmail.com>
2016-11-12 22:44:49 -0600

    Add port mapping

--- host-notebook.sh
+++ host-notebook.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose run web /bin/bash /usr/src/app/turkey/run-notebook-server.sh;
+docker-compose run -p 8888:8888 web /bin/bash /usr/src/app/turkey/run-notebook-server.sh;
