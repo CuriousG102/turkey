efbbb6415a7e7f4dcee48979b0668856d1167794
Miles Hutson <mileshutson@gmail.com>
2016-12-10 14:31:48 -0600

    Add extension

--- dump-data.sh
+++ dump-data.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker-compose -f docker-compose-science.yml build;
-(docker-compose run -e SECRET_KEY=whocares web python /usr/src/app/turkey/manage.py dumpdata) > databasefile;
+(docker-compose run -e SECRET_KEY=whocares web python /usr/src/app/turkey/manage.py dumpdata) > databasefile.json;
